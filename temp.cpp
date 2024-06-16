80039efd32bd34e4a310359eddc48a83c67eeb78
hprogq <hhq@live.com>
2024-06-16 14:40:37 +0800

    Test Commit

--- temp.cpp
+++ temp.cpp
@@ -1,13 +1,11 @@
+// Temp File
 #include <iostream>
 using namespace std;
-const int N = 99999;
 int main() {
 #ifdef LOCAL
     freopen("temp.in", "r", stdin);
     freopen("temp.out", "w", stdout);
 #endif
-    printf("%d\n", N);
-    for ( int i = 1; i < N; i ++ )
-        printf("%d ", i + 1);
-    printf("%d\n", 1);
+    ;
+    return 0;
 }
